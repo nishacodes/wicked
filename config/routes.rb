e0ee1e5d2c0c3d4508a9b7563dbb7d1b67ebcf9b
nishacodes <nishabatra27@gmail.com>
2014-08-15 23:38:05 -0400

    add devise and create pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Wicked::Application.routes.draw do
+  
+  devise_for :users
+
+  root to: "pages#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
