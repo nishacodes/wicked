2b1343f5d49b7796087e0367c886f87ca944f8a0
ajosepha <ariel.isaacs@gmail.com>
2014-08-16 17:46:41 -0400

    small changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+json = ActiveSupport::JSON.decode(File.read('db/wicrules.json'))
+ 
+json.each do |a|
+  Wicrule.create!(a['wicrule'], without_protection: true)
+end
