31c8f48a78b554cc50e108c951302ee1a5fb13fe
nishacodes <nishabatra27@gmail.com>
2014-08-17 09:59:40 -0400

    response with something when UPC is given

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -7,43 +7,51 @@ class VerificationsController < ApplicationController
     @message = Message.create(:body=>params["Body"].downcase, :image=>params["MediaUrl"])
 
     # @message.body = params["Body"].downcase
-    @from_phone = params["Phone"]
+    @phone = params["Phone"]
 
     # Instantiate a Twilio client
     @client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
-
     
     # New user verification
     if @user && @user.verified==nil 
+      @phone = @user.phone
       if @message.body == "yes" 
         @user.update_attribute(:verified, true)
-        @client.account.sms.messages.create(
-            from: TWILIO_CONFIG['from'],
-            to: @user.phone,
-            body: "Thanks! You're all set. Now you can send me a barcode or UPC number to see if it's WIC approved."
-          )
+        @response = "Thanks! You're all set. Now you can send me a barcode or UPC number to see if it's WIC approved."
+        # @client.account.sms.messages.create(
+        #     from: TWILIO_CONFIG['from'],
+        #     to: @user.phone,
+        #     body: "Thanks! You're all set. Now you can send me a barcode or UPC number to see if it's WIC approved."
+        #   )
       else
-        @client.account.sms.messages.create(
-          from: TWILIO_CONFIG['from'],
-          to: @user.phone,
-          body: "Sorry, that is not a valid response. Please say 'yes' to verify your account."
-        )
+        @response = "Sorry, that is not a valid response. Please say 'yes' to verify your account."
+        # @client.account.sms.messages.create(
+        #   from: TWILIO_CONFIG['from'],
+        #   to: @user.phone,
+        #   body: "Sorry, that is not a valid response. Please say 'yes' to verify your account."
+        # )
       end
+    
     # Receiving any message from verified user    
     elsif @user && @user.verified
+      @phone = @user.phone
       @message.get_upc
+      # if it's a upc number, get the info from upc api
       if @message.upc
-        # print "upc is: " + @message.upc    
+        puts "upc is: " + @message.upc    
         # 689544080008  
         uri = URI("http://api.simpleupc.com/v1.php?auth=gqA1YvUeSLhWA6h5dvICvzUaYe3Dff8M&method=FetchProductByUPC&upc=#{@message.upc}&returnFormat=json")
         res = Net::HTTP.get_response(uri)
+        
+        # if there's results, save it in the items database
         if res.body
           item_info = JSON.parse(res.body)["result"]
           @item = Item.create(item_info)
           
-          print "***************"
-          print @item.brand 
-          print @item.category
+
+          puts "-----"
+          puts @item.brand 
+          puts @item.category
           # save nutrition info in a hash
           if @item.ProductHasNutritionFacts == true
             nutritionUri = URI("http://api.simpleupc.com/v1.php?auth=gqA1YvUeSLhWA6h5dvICvzUaYe3Dff8M&method=FetchNutritionFactsByUPC&upc=#{@item.upc}")
@@ -55,28 +63,42 @@ class VerificationsController < ApplicationController
           if @item.ProductHasImage == true
             imageUri = URI("http://api.simpleupc.com/v1.php?auth=gqA1YvUeSLhWA6h5dvICvzUaYe3Dff8M&method=FetchImageByUPC&upc=#{@item.upc}")
             image = Net::HTTP.get_response(imageUri)
-            # @item.image = JSON.parse(image.body)["result"]["image"]
-            print "****----****"
+
             result = JSON.parse(image.body)["result"]
 
             @item.image = result["imageURL"]
             @item.imagethumb = result["imageThumbURL"]
             @item.save
-
-            print @item.image
-            # print image.body
-            # @item.nutrition = JSON.parse(nutritionfacts.body)["result"]
           end
         end
 
+        # if an item got saved, prep the response
+        if (@item.brand && @item.description)
+          @itemRequest = @item.brand + " " + @item.description
+          @response = "You requested #{@itemRequest}. Let me see if that is approved..."
+          # INSERT LOGIC FOR DETERMINING IF IT IS WIC APPROVED 
+          # @response =
+        else
+          @response = "Hmm, I could not find an item with that UPC number. Please try again."
+        end
 
-
+      # DETERMINE RESPONSE WITH GENERIC REQUEST
       elsif @message.item_requested
-        print "item is: " + @message.item_requested    
+        
       end
       
-
+      # Send this response after parsing
+      puts @response
     end
+
+    puts "*********"
+    puts @phone
+    puts @response
+    @client.account.sms.messages.create(
+            from: TWILIO_CONFIG['from'],
+            to: @phone,
+            body: @response
+      )
     head :ok
   end
 
